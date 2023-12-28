1b0ef7df457de01cdcb285501813231b64f61110
ANTHONYCCOLQUE <104391632+ANTHONYCCOLQUE@users.noreply.github.com>
2023-12-27 22:14:16 -0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <header className='header'>
             <Navbar />
             <div className='header-content flex flex-c text-center text-white'>
-                <h2 className='header-title text-capitalize'>Search Books</h2><br />
+                <h2 className='header-title text-capitalize'>Buscar Libros</h2><br />
                 <p className='header-text fs-18 fw-3'></p>
                 <SearchForm />
             </div>
